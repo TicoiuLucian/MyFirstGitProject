bd1776d4ea6d427b88375f2139f47f91523a768c
CostinMP <costin.mp@yahoo.com>
2022-02-24 19:46:48 +0200

    sout hello_team

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world !");
+
+        System.out.println("Hello team!");
     }
+
+
 }
