e872c410b2884e36ef2899441f0d947ff77e92f7
marianRusu1 <rusumarian.ro@gmail.com>
2022-02-28 22:28:07 +0200

    createArmadilloEntity

--- src/main/java/ro/itschool/entity/Armadillo.java
+++ src/main/java/ro/itschool/entity/Armadillo.java
@@ -1,5 +1,26 @@
 package ro.itschool.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "armadillo")
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Armadillo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = ("name_of_armadillo"))
+    private String name;
+
+    @Column(name = ("age_of_armadillo"))
+    private short age;
 }
