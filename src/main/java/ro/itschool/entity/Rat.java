c21d02122c6674ca56fa411a2f2026d55b4382e7
alex <colo.alexandru@yahoo.com>
2022-03-01 18:11:59 +0200

    added rat into entity

---
+++ src/main/java/ro/itschool/entity/Rat.java
@@ -0,0 +1,18 @@
+package ro.itschool.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Rat {
+    private static int id;
+    private static String name;
+    private static String weight;
+
+
+}
